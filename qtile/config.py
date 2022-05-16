62f9d03779bf6f4508efdf59af28da85488e56ed
Aiden Schulman <aidenhschulman@gmail.com>
2022-05-15 21:59:23 -0400

    cleaned up config.py in qtile directory

--- qtile/config.py
+++ qtile/config.py
@@ -1,40 +1,46 @@
-# Copyright (c) 2010 Aldo Cortesi
-# Copyright (c) 2010, 2014 dequis
-# Copyright (c) 2012 Randall Ma
-# Copyright (c) 2012-2014 Tycho Andersen
-# Copyright (c) 2012 Craig Barnes
-# Copyright (c) 2013 horsik
-# Copyright (c) 2013 Tao Sauvage
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
+import os
+import subprocess
+from typing import List  # noqa: F401
 
-from libqtile import bar, layout, widget
-from libqtile.config import Click, Drag, Group, Key, Match, Screen
+from libqtile import hook
+
+from libqtile.extension.dmenu import DmenuRun
+from libqtile.extension.window_list import WindowList
+from libqtile.extension.command_set import CommandSet
+
+# import layout objects
+from libqtile.layout.columns import Columns
+from libqtile.layout.xmonad import MonadTall
+from libqtile.layout.stack import Stack
+from libqtile.layout.floating import Floating
+
+# import widgets and bar
+
+from libqtile.config import Click, Drag, DropDown, Group, Key, Match, ScratchPad, Screen
 from libqtile.lazy import lazy
-from libqtile.utils import guess_terminal
+
+# from libqtile.utils import guess_terminal
+
+from colors import catppuccin_morning, catppuccin_night
+
+from bar_transparent_rounded import bar1, bar2
+
 
 mod = "mod4"
-terminal = guess_terminal()
+terminal = "alacritty"
+# terminal = guess_terminal()
 
 keys = [
-    # A list of available commands that can be bound to keys can be found
-    # at https://docs.qtile.org/en/latest/manual/config/lazy.html
+    # Command Prompt
+    Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
+    # Toggle floating and fullscreen
+    Key([mod], "f", lazy.window.toggle_fullscreen(), desc="Toggle fullscreen mode"),
+    Key(
+        [mod, "shift"],
+        "space",
+        lazy.window.toggle_floating(),
+        desc="Toggle floating mode",
+    ),
     # Switch between windows
     Key([mod], "Left", lazy.layout.left(), desc="Move focus to left"),
     Key([mod], "Right", lazy.layout.right(), desc="Move focus to right"),
@@ -95,15 +101,13 @@ keys = [
 
 # groups = [Group(i) for i in "123456789"]
 groups = [
-    Group(name="1", label="", matches=[Match(wm_class="firefox")], layout="max"),
-    Group(name="2", label="", matches=[Match(wm_class="code-oss")], layout="columns"),
-    Group(name="3", label="", matches=[Match(wm_class="Steam")], layout="max"),
+    Group(name="1", label="", matches=[Match(wm_class="firefox")], layout="stack"),
     Group(
-        name="4",
-        label="",
-        matches=[Match(wm_class="discord"), Match(wm_class="spotify")],
-        layout="columns",
+        name="2", label="", matches=[Match(wm_class="code-oss")], layout="monadtall"
     ),
+    Group(name="3", label="", matches=[Match(wm_class="Steam")], layout="max"),
+    Group(name="4", label="", matches=[Match(wm_class="discord")], layout="columns"),
+    Group(name="5", label="嗢", matches=[Match(wm_class="vlc")], layout="columns"),
 ]
 
 for i in groups:
@@ -127,72 +131,45 @@ for i in groups:
     )
 
 layouts = [
-    layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=4),
-    layout.Max(),
-    # Try more layouts by unleashing below layouts.
-    # layout.Stack(num_stacks=2),
-    # layout.Bsp(),
-    # layout.Matrix(),
-    # layout.MonadTall(),
-    # layout.MonadWide(),
-    # layout.RatioTile(),
-    # layout.Tile(),
-    # layout.TreeTab(),
-    # layout.VerticalTile(),
-    # layout.Zoomy(),
-    layout.floating.Floating(),
+    Stack(
+        border_normal=catppuccin_night["gray0"],
+        border_focus=catppuccin_morning["blue"],
+        border_width=2,
+        num_stacks=1,
+        margin=10,
+    ),
+    MonadTall(
+        border_normal=catppuccin_night["gray0"],
+        border_focus=catppuccin_morning["blue"],
+        margin=10,
+        border_width=2,
+        single_border_width=2,
+        single_margin=10,
+    ),
+    Columns(
+        border_normal=catppuccin_night["gray0"],
+        border_focus=catppuccin_morning["blue"],
+        border_width=2,
+        border_normal_stack=catppuccin_night["gray0"],
+        border_focus_stack=catppuccin_morning["sky"],
+        border_on_single=2,
+        margin=10,
+        margin_on_single=10,
+    ),
 ]
 
-widget_defaults = dict(
-    font="Caskaydia Cove Nerd Font",
-    fontsize=16,
-    padding=3,
+floating_layout = Floating(
+    border_normal=catppuccin_night["gray0"],
+    border_focus=catppuccin_morning["red"],
+    border_width=3,
+    float_rules=[
+        *Floating.default_float_rules,
+        Match(wm_class="blueman-manager"),
+        Match(wm_class="pavucontrol"),
+        Match(wm_class="bitwarden"),
+        Match(wm_class="thunar"),
+    ],
 )
-extension_defaults = widget_defaults.copy()
-
-screens = [
-    Screen(
-        wallpaper="~/.config/qtile/background.png",
-        wallpaper_mode="stretch",
-        top=bar.Bar(
-            [
-                widget.GroupBox(highlight_method="text", hide_unused=False),
-                widget.CurrentLayout(),
-                widget.Prompt(),
-                widget.WindowName(),
-                # widget.TextBox("ahs", name="default"),
-                widget.Systray(),
-                widget.Clock(format="%m-%d-%Y %a %I:%M %p"),
-                widget.QuickExit(default_text="[X]", countdown_format="[{}]"),
-            ],
-            24,
-            # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
-            # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
-        ),
-    ),
-    Screen(
-        wallpaper="~/.config/qtile/background.png",
-        wallpaper_mode="stretch",
-        top=bar.Bar(
-            [
-                widget.CurrentLayout(),
-                widget.GroupBox(highlight_method="text", hide_unused=False),
-                widget.WindowName(),
-                # widget.TextBox("ahs", name="default"),
-                widget.NvidiaSensors(),
-                widget.CPU(),
-                widget.CPUGraph(type="line", line_width=1),
-                widget.Memory(),
-                widget.MemoryGraph(type="line", line_width=1),
-                widget.Net(interface="enp4s0"),
-                widget.NetGraph(),
-            ],
-            24,
-            # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
-            # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
-        ),
-    ),
-]
 
 # Drag floating layouts.
 mouse = [
@@ -208,40 +185,35 @@ mouse = [
     Click([mod], "Button2", lazy.window.bring_to_front()),
 ]
 
+widget_defaults = dict(
+    font="Caskaydia Cove Nerd Font",
+    fontsize=16,
+    padding=10,
+    foreground=catppuccin_night["black1"],
+)
+
+extension_defaults = widget_defaults.copy()
+
+screens = [
+    Screen(
+        wallpaper="~/.config/qtile/background.png",
+        wallpaper_mode="stretch",
+        top=bar1,
+    ),
+    Screen(
+        wallpaper="~/.config/qtile/background.png",
+        wallpaper_mode="stretch",
+        top=bar2,
+    ),
+]
+
 dgroups_key_binder = None
 dgroups_app_rules = []  # type: list
 follow_mouse_focus = True
-bring_front_click = False
+bring_front_click = ""
 cursor_warp = False
-floating_layout = layout.Floating(
-    float_rules=[
-        # Run the utility of `xprop` to see the wm class and name of an X client.
-        *layout.Floating.default_float_rules,
-        Match(wm_class="confirmreset"),  # gitk
-        Match(wm_class="makebranch"),  # gitk
-        Match(wm_class="maketag"),  # gitk
-        Match(wm_class="ssh-askpass"),  # ssh-askpass
-        Match(title="branchdialog"),  # gitk
-        Match(title="pinentry"),  # GPG key password entry
-    ]
-)
 auto_fullscreen = True
 focus_on_window_activation = "smart"
 reconfigure_screens = True
-
-# If things like steam games want to auto-minimize themselves when losing
-# focus, should we respect this or not?
 auto_minimize = True
-
-# When using the Wayland backend, this can be used to configure input devices.
-wl_input_rules = None
-
-# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
-# string besides java UI toolkits; you can see several discussions on the
-# mailing lists, GitHub issues, and other WM documentation that suggest setting
-# this string if your java app doesn't work correctly. We may as well just lie
-# and say that we're a working one by default.
-#
-# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
-# java that happens to be on java's whitelist.
 wmname = "LG3D"
