b1656645b649c60e31270e6dd9d14c3cdca20e7e
Aiden Schulman <aidenhschulman@gmail.com>
2022-06-06 17:07:16 -0400

    Added Doom Emacs config file

---
+++ .doom.d/config.el
@@ -0,0 +1,11 @@
+(setq user-full-name "John Doe"
+      user-mail-address "john@doe.com")
+
+(setq doom-font (font-spec :family "CaskaydiaCove Nerd Font" :size 17)
+      doom-variable-pitch-font (font-spec :family "CaskaydiaCove Nerd Font" :size 17))
+
+(setq doom-theme 'doom-tokyo-night)
+
+(setq display-line-numbers-type t)
+
+(setq org-directory "~/org/")
