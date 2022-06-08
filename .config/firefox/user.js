ad2a26e2d406c578c865e2e1a1cba0b765cf2415
Aiden Schulman <aidenhschulman@gmail.com>
2022-06-08 02:04:27 -0400

    Added additional privacy config settings

--- .config/firefox/user.js
+++ .config/firefox/user.js
@@ -1,4 +1,5 @@
-/** Privacy Config Settings **/
+// Privacy Config Settings
+// Level 1
 user_pref("browser.newtabpage.activity-stream.feeds.telemetry", false);	// default is true
 user_pref("browser.ping-centre.telemetry", false);	// default is true
 user_pref("browser.tabs.crashReporting.sendReport", false);	// default is true
@@ -13,15 +14,27 @@ user_pref("browser.newtabpage.activity-stream.showSponsored", false);	// default
 user_pref("extensions.pocket.enabled", false);	// default is true
 user_pref("network.dns.disablePrefetch", true);	// default is false
 user_pref("network.prefetch-next", false);	// default is true
+user_pref("pdfjs.enableScripting", false); // default is true
+user_preF("security.ssl.require_safe_negotiation", true);
 user_pref("identity.fxaccounts.enabled", false);	// default is true
+
+// Level 2
 user_pref("geo.enabled", false);	// default is true
 user_pref("dom.webnotifications.enabled", false);	// default is true
-user_pref("brower.sessionstore.resume_from_crash", false);	// default is true
+user_pref("media.peerconnection.enabled", false);
+user_pref("media.navigator.enabled", false);
+user_pref("webgl.disabled", true);
 
-/**  CSS Theme Settings	  **/
+// Level 3
+user_pref("privacy.resistFingerprinting", true);
+user_pref("network.cookie.lifetimePolicy", true);
+
+// CSS Theme Settings
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true); // default is false
 user_pref("svg.context-properties.content.enabled", true); 		// default is false
 user_pref("browser.tabs.tabClipWidth", 83);				// default is 140
+
+// Convenience Settings
 user_pref("browser.sessionstore.resume_from_crash", false); // default is true
 user_pref("browser.urlbar.trimURLs", false); // default is true
 user_pref("browser.urlbar.update2.engineAliasRefresh", true); // default is false
