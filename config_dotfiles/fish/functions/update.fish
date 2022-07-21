822cfcc581dfc36188f7f2690cd10b2e1ea3602d
Aiden Schulman <aidenhschulman@gmail.com>
2022-07-21 11:57:32 -0400

    Updated print statements

--- config_dotfiles/fish/functions/update.fish
+++ config_dotfiles/fish/functions/update.fish
@@ -1,10 +1,10 @@
-function update -d "update brew, fish, fisher and mac app store"
-    echo 'Updating homebrew'
+function update -d "Update brew and mac app store"
+    echo 'Updating homebrew...'
     brew update
     brew upgrade
     brew cleanup
 
-    echo 'Checking Apple Updates'
+    echo 'Checking Apple Updates...'
     /usr/sbin/softwareupdate -ia
 
     exit 0
