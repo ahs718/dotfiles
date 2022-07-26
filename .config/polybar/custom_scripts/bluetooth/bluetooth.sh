53d6937bb2ae5e9e7d763a803379116defe8d136
Aiden Schulman <aidenhschulman@gmail.com>
2022-07-26 18:47:50 -0400

    Modified directories

---
+++ .config/polybar/custom_scripts/bluetooth/bluetooth.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+if [ $(bluetoothctl show | grep "Powered: yes" | wc -c) -eq 0 ]
+then
+  echo ""
+else
+  connected=$(python $HOME/.config/polybar/custom_scripts/bluetooth/script.py)
+  if [ -z "${connected}" ];
+  then
+    echo ""
+  fi
+  echo " %{T1}$connected%{T-}"
+fi
