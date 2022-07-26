c6a67475367d8ec5d59f357effd023a60d8987a4
Aiden Schulman <aidenhschulman@gmail.com>
2022-07-26 19:59:48 -0400

    Removed feh wallpaper command

--- .config/bspwm/start.sh
+++ .config/bspwm/start.sh
@@ -1,6 +1,5 @@
 /usr/lib/polkit-kde-authentication-agent-1&
 xrandr --output DVI-D-0 --primary --mode 1920x1080 --pos 1962x0 --rotate normal --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-0 --off --output DP-1 --off &
-feh --no-fehbg -bg-scale '/home/ahs/.images/yosemite2.jpg'
 xsetroot -cursor_name left_ptr
 setxkbmap us &
 picom -b &
