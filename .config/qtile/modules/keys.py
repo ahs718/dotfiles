88fc3e1726eb8c8388ebe9b70be9143867b21e13
Aiden Schulman <aidenhschulman@gmail.com>
2022-07-27 16:52:00 -0400

    Added keybinding file

---
+++ .config/qtile/modules/keys.py
@@ -0,0 +1,13 @@
+from libqtile.config import Key
+from libqtile.lazy import lazy
+
+import os
+import subprocess
+
+# Assign keys and home directory values to variables
+mod = "mod4"
+control = "control"
+shift = "shift"
+alt = "mod1"
+terminal = "kitty"
+home = os.path.expanduser('~')
