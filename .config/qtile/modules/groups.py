e1ae61712453ca18083c1180cde84ad40075ad1f
Aiden Schulman <aidenhschulman@gmail.com>
2022-07-27 17:21:44 -0400

    Added whitespace

--- .config/qtile/modules/groups.py
+++ .config/qtile/modules/groups.py
@@ -1,5 +1,6 @@
 from libqtile.config import Group, Key, Match, ScratchPad, DropDown
 from libqtile.lazy import lazy
+
 from utils.settings import workspace_names
 from modules.keys import keys, mod, shift
 
