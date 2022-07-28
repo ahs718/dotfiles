7687a558d8dbb676aa9705efa0a2178f73be2547
Aiden Schulman <aidenhschulman@gmail.com>
2022-07-28 18:00:25 -0400

    Moved config files

--- config/qtile/scripts/config_xrandr.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# monitor config
-xrandr --output DVI-D-1 --mode 1920x1080 --pos 1967x0 --rotate normal --output HDMI-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off
-xsetroot -cursor_name left_ptr
