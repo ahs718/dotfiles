faf6ad605b538fcc8cd488d9a0c3953a5b7b4d38
Aiden Schulman <aidenhschulman@gmail.com>
2022-07-28 17:57:06 -0400

    Deleted unused config files

--- config/bspwm/start.sh
+++
@@ -1,5 +0,0 @@
-/usr/lib/polkit-kde-authentication-agent-1&
-xrandr --output DVI-D-0 --primary --mode 1920x1080 --pos 1962x0 --rotate normal --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-0 --off --output DP-1 --off &
-xsetroot -cursor_name left_ptr
-setxkbmap us &
-picom -b &
