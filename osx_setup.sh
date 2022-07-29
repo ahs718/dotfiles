116adf68a4559cf1c195838a216a1a3f0faa59a2
Aiden Schulman <aidenhschulman@gmail.com>
2022-07-29 17:42:57 -0400

    Changed symlinks to copy commands for some folders

--- osx_setup.sh
+++ osx_setup.sh
@@ -13,6 +13,7 @@ sudo rm -rf ~/.xinitrc >/dev/null 2>&1
 sudo rm -rf ~/.images >/dev/null 2>&1
 
 sudo rm -rf ~/.config/ >/dev/null 2>&1
+sudo rm -rf ~/.config/{bat,btop,fish,neofetch,nvim} >/dev/null 2>&1
 
 # Create symlinks in the home & .config folder
 # Comment out the config files you do not want to symlink
@@ -23,9 +24,9 @@ ln -sf $dotfiles_dir/vim/.vimrc ~/.vimrc && echo "added .vimrc"
 ln -sf $dotfiles_dir/zsh/.zshrc ~/.zshrc && echo "added .zshrc"
 
 ln -sf $dotfiles_dir/bat ~/.config/bat && echo "added bat files"
-ln -sf $dotfiles_dir/btop ~/.config/btop && echo "added btop files"
+cp -r $dotfiles_dir/btop ~/.config/btop && echo "added btop files"
 # ln -sf $dotfiles_dir/doom-emacs ~/.doom.d && echo "added doom-emacs files"
-ln -sf $dotfiles_dir/fish ~/.config/fish && echo "added fish files"
+cp -r $dotfiles_dir/fish ~/.config/fish && echo "added fish files"
 ln -sf $dotfiles_dir/images ~/.images && echo "added images"
 ln -sf $dotfiles_dir/nvim ~/.config/nvim && echo "added neovim files"
 ln -sf $dotfiles_dir/neofetch ~/.config/neofetch && echo "added neofetch files"
