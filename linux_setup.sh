ead08e853e47b57be1130b495b37a00333e31edb
Aiden Schulman <aidenhschulman@gmail.com>
2022-07-29 16:20:17 -0400

    Removed latte dock install option, changed removing entire .config directory to just removing specific applications

--- linux_setup.sh
+++ linux_setup.sh
@@ -12,11 +12,12 @@ sudo rm -rf ~/.gitconfig >/dev/null 2>&1
 sudo rm -rf ~/.xinitrc >/dev/null 2>&1
 sudo rm -rf ~/.images >/dev/null 2>&1
 
-sudo rm -rf ~/.config/ >/dev/null 2>&1
+sudo rm -rf ~/.config/{Typora,bat,btop,fish,kitty,latte,neofetch,nvim,picom,konsole,alacritty} >/dev/null 2>&1
+
+mkdir -p ~/.config >/dev/null 2>&1
 
 # Create symlinks in the home & .config folder
 # Comment out the config files you do not want to symlink
-mkdir ~/.config
 ln -sf $dotfiles_dir/bash/.bashrc ~/.bashrc && echo "added .bashrc"
 ln -sf $dotfiles_dir/git/.gitconfig ~/.gitconfig && echo "added .gitconfig"
 ln -sf $dotfiles_dir/vim/.vimrc ~/.vimrc && echo "added .vimrc"
@@ -31,7 +32,6 @@ rm ~/.config/fish/functions/code.fish && echo "added fish files"
 ln -sf $dotfiles_dir/images ~/.images && echo "added images"
 ln -sf $dotfiles_dir/kitty ~/.config/kitty && echo "added kitty terminal files"
 # ln -sf $dotfiles_dir/konsole ~/.config/konsole && echo "added konsole files"
-ln -sf $dotfiles_dir/latte ~/.config/latte && echo "added latte dock files"
 ln -sf $dotfiles_dir/neofetch ~/.config/neofetch && echo "added neofetch files"
 ln -sf $dotfiles_dir/nvim ~/.config/nvim && echo "added neovim files"
 # ln -sf $dotfiles_dir/picom ~/.config/picom && echo "added picom files"
