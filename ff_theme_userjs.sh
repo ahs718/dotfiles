048b2b7194e2951ff69e328ef5807229f68a00e0
Aiden Schulman <aidenhschulman@gmail.com>
2022-07-28 22:22:58 -0400

    Added firefox theme and user.js install script

---
+++ ff_theme_userjs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Variables
+dotfiles_dir=$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)
+
+echo "Installing MaterialFox theme..."
+git clone https://github.com/muckSponge/MaterialFox.git ~/MaterialFox
+cp -r ~/MaterialFox/chrome ~/.mozilla/firefox/*.default-release
+rm -rf ~/MaterialFox
+cp $dotfiles_dir/firefox/user.js ~/.mozilla/firefox*.default-release
+echo "Firefox css theme and user.js file set!"
