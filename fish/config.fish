bcf2a14f1e97f9ad9e64535670d4b06ee2137822
Aiden Schulman <aidenhschulman@gmail.com>
2022-08-04 11:36:44 -0400

    Updated prompt

--- fish/config.fish
+++ fish/config.fish
@@ -2,5 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     set fish_greeting
     set -gx EDITOR nvim
+    export STARSHIP_CONFIG=~/.config/starship/starship.toml
     starship init fish | source
 end
