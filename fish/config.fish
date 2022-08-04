b027ec90764fa0d4b6b70fdc1b127c071207e1eb
Aiden Schulman <aidenhschulman@gmail.com>
2022-08-04 11:45:01 -0400

    Removed custom config dir

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,5 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     set fish_greeting
     set -gx EDITOR nvim
-    export STARSHIP_CONFIG=~/.config/starship/starship.toml
     starship init fish | source
 end
