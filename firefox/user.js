ce423083bb373fd967efd01849e70524784f07dd
Aiden Schulman <aidenhschulman@gmail.com>
2022-08-16 16:23:20 -0400

    Added firefox hardware acceleration config settings on linux

--- firefox/user.js
+++ firefox/user.js
@@ -1,26 +1,32 @@
 // Privacy Config Settings
 // Level 1
-user_pref("browser.newtabpage.activity-stream.feeds.telemetry", false);	// default is true
-user_pref("browser.ping-centre.telemetry", false);	// default is true
-user_pref("browser.tabs.crashReporting.sendReport", false);	// default is true
-user_pref("devtools.onboarding.telemetry.logged", false);	// default is true
-user_pref("toolkit.telemetry.enabled", false);	// default is true
+user_pref("browser.newtabpage.activity-stream.feeds.telemetry", false); // default is true
+user_pref("browser.ping-centre.telemetry", false); // default is true
+user_pref("browser.tabs.crashReporting.sendReport", false); // default is true
+user_pref("devtools.onboarding.telemetry.logged", false); // default is true
+user_pref("toolkit.telemetry.enabled", false); // default is true
 user_pref("toolkit.telemetry.server", "");
-user_pref("toolkit.telemetry.unified", false);	// default is true
-user_pref("browser.newtabpage.activity-stream.feeds.discoverystreamfeed", false);	// default is true
-user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);	// default is true
-user_pref("browser.newtabpage.activity-stream.section.highlights.includePocket", false);	// default is true
-user_pref("browser.newtabpage.activity-stream.showSponsored", false);	// default is true
-user_pref("extensions.pocket.enabled", false);	// default is true
-user_pref("network.dns.disablePrefetch", true);	// default is false
-user_pref("network.prefetch-next", false);	// default is true
+user_pref("toolkit.telemetry.unified", false); // default is true
+user_pref(
+  "browser.newtabpage.activity-stream.feeds.discoverystreamfeed",
+  false
+); // default is true
+user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false); // default is true
+user_pref(
+  "browser.newtabpage.activity-stream.section.highlights.includePocket",
+  false
+); // default is true
+user_pref("browser.newtabpage.activity-stream.showSponsored", false); // default is true
+user_pref("extensions.pocket.enabled", false); // default is true
+user_pref("network.dns.disablePrefetch", true); // default is false
+user_pref("network.prefetch-next", false); // default is true
 user_pref("pdfjs.enableScripting", false); // default is true
 user_preF("security.ssl.require_safe_negotiation", true);
-user_pref("identity.fxaccounts.enabled", false);	// default is true
+user_pref("identity.fxaccounts.enabled", false); // default is true
 
 // Level 2
-user_pref("geo.enabled", false);	// default is true
-user_pref("dom.webnotifications.enabled", false);	// default is true
+user_pref("geo.enabled", false); // default is true
+user_pref("dom.webnotifications.enabled", false); // default is true
 user_pref("media.peerconnection.enabled", false);
 user_pref("media.navigator.enabled", false);
 user_pref("webgl.disabled", true);
@@ -31,10 +37,19 @@ user_pref("network.cookie.lifetimePolicy", true);
 
 // CSS Theme Settings
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true); // default is false
-user_pref("svg.context-properties.content.enabled", true); 		// default is false
-user_pref("browser.tabs.tabClipWidth", 83);				// default is 140
+user_pref("svg.context-properties.content.enabled", true); // default is false
+user_pref("browser.tabs.tabClipWidth", 83); // default is 140
 
 // Convenience Settings
 user_pref("browser.sessionstore.resume_from_crash", false); // default is true
 user_pref("browser.urlbar.trimURLs", false); // default is true
 user_pref("browser.urlbar.update2.engineAliasRefresh", true); // default is false
+
+// user_pref("media.ffmpeg.vaapi.enabled", true);
+// user_pref("media.rdd-ffmpeg.enabled", true);
+// user_pref("media.av1.enabled", false);
+// user_pref("gfx.x11-egl.force-enabled", true);
+// user_pref("widget.dmabuf.force-enabled", true);
+// user_pref("widget.use-xdg-desktop-portal.mime-handler", 1);
+// user_pref("widget.use-xdg-desktop-portal.file-picker", 1);
+// user_pref("media.hardwaremediakeys.enabled", false);
